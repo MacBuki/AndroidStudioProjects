a869ba7babb26c6f21d4150f2b8fcce14a5bc3a3
Ken Jeon <44134072+MacBuki@users.noreply.github.com>
2024-05-16 11:01:10 +0900

    may16up

---
+++ May16th/app/src/main/java/androidtown/org/may16th/MainActivity.java
@@ -0,0 +1,14 @@
+package androidtown.org.may16th;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
