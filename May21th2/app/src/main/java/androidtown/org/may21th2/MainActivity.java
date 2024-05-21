55a1e159348bd57b7ecfa82dda19512240277326
Ken Jeon <44134072+MacBuki@users.noreply.github.com>
2024-05-21 12:26:23 +0900

    비디오 버튼 추가

--- May21th2/app/src/main/java/androidtown/org/may21th2/MainActivity.java
+++ May21th2/app/src/main/java/androidtown/org/may21th2/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.VideoView;
-
 public class MainActivity extends AppCompatActivity {
     static final String VIDEO_URL = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
     private VideoView videoView;
@@ -25,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button startBtn = (Button) findViewById(R.id.startBtn);
         Button volumeBtn = (Button) findViewById(R.id.volumeBtn);
+        Button volumeHalfBtn = (Button) findViewById(R.id.volumeHalfBtn);
+        Button muteBtn = (Button) findViewById(R.id.muteBtn);
 
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +41,23 @@ public class MainActivity extends AppCompatActivity {
                 AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
                 int maxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
                 mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_SHOW_UI);
+            }
+        });
+
+        volumeHalfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                int halfVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) / 2;
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, halfVolume, AudioManager.FLAG_SHOW_UI);
+            }
+        });
+
+        muteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, AudioManager.FLAG_SHOW_UI);
             }
         });
     }
