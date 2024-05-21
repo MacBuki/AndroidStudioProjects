144c299e2b7898e0717918aa830f915d12ab083a
Ken Jeon <44134072+MacBuki@users.noreply.github.com>
2024-05-21 12:15:29 +0900

    upup

--- May21th/app/src/main/java/androidtown/org/may21th/MainActivity.java
+++ May21th/app/src/main/java/androidtown/org/may21th/MainActivity.java
@@ -3,12 +3,78 @@ package androidtown.org.may21th;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    static final String AUDIO_URL = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+    private MediaPlayer mediaPlayer;
+    private int playbackPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = (Button) findViewById(R.id.plyBtn);
+        Button pauseBtn = (Button) findViewById(R.id.pauseBtn);
+        Button restartBtn = (Button) findViewById(R.id.restartBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                try{
+                    playAudio(AUDIO_URL);
+                    Toast.makeText(getApplicationContext(),"음악 파일 재생 시작됨.", Toast.LENGTH_LONG).show();
+                } catch (Exception e){e.printStackTrace();}
+            }
+        });
+
+        pauseBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer !=null){
+                    playbackPosition = mediaPlayer.getCurrentPosition();
+                    mediaPlayer.pause();
+                    Toast.makeText(getApplicationContext(),"음악 파일 재생 중지됨.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        restartBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer != null && !mediaPlayer.isPlaying()){
+                    mediaPlayer.start();
+                    mediaPlayer.seekTo(playbackPosition);
+                    Toast.makeText(getApplicationContext(),"음악 파일 재생 재시작됨.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private void playAudio(String url) throws Exception {
+        killMediaPlayer();
+        mediaPlayer=new MediaPlayer();
+        mediaPlayer.setDataSource(url);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        killMediaPlayer();
+    }
+
+    private void killMediaPlayer(){
+        if(mediaPlayer!=null){
+            try{
+                mediaPlayer.release();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
