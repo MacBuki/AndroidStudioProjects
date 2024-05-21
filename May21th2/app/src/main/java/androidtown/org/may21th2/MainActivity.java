18d0b49741c825e6477b4227e5a451cdf412baf9
Ken Jeon <44134072+MacBuki@users.noreply.github.com>
2024-05-21 12:20:27 +0900

    video_example

--- May21th2/app/src/main/java/androidtown/org/may21th2/MainActivity.java
+++ May21th2/app/src/main/java/androidtown/org/may21th2/MainActivity.java
@@ -1,14 +1,46 @@
 package androidtown.org.may21th2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.media.AudioManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+    static final String VIDEO_URL = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+        MediaController mc = new MediaController(this);
+        videoView.setMediaController(mc);
+        videoView.setVideoURI(Uri.parse(VIDEO_URL));
+
+        Button startBtn = (Button) findViewById(R.id.startBtn);
+        Button volumeBtn = (Button) findViewById(R.id.volumeBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                videoView.seekTo(0);
+                videoView.start();
+            }
+        });
+
+        volumeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioManager mAudioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+                int maxVolume = mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, AudioManager.FLAG_SHOW_UI);
+            }
+        });
     }
 }
